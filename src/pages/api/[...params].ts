22f0d4d02deead7f6cb87a5fc7ebcd7299c15f68
Calvin Smoot <calvin@xeosmoot.com>
2022-06-27 11:24:49 -0700

    fixing other endpoint

--- src/pages/api/[...params].ts
+++ src/pages/api/[...params].ts
@@ -1,8 +1,7 @@
+import { PrismaClient } from '@prisma/client';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import {
   ResourcesType,
-  getCollection,
-  getSingleDoc,
   Planet,
   Film,
   People,
@@ -10,11 +9,12 @@ import {
   Vehicle,
   Species,
 } from '../../utils';
+const prisma = new PrismaClient();
 
-export default function userHandler(
+export default async function userHandler(
   req: NextApiRequest,
   res: NextApiResponse<any>
-): void {
+): Promise<void> {
   const { params, search } = req.query;
   if (!params || !Array.isArray(params)) {
     res.status(404).end(`Path not found`);
@@ -28,50 +28,20 @@ export default function userHandler(
     switch (path) {
       case ResourcesType.Planets:
         if (id !== null) {
-          getOne<Planet>(path, id, res);
+          const planet = await prisma.planet.findFirst({
+            where: { id: Number(id) },
+          });
+          const json = JSON.stringify(planet, (_, value) =>
+            typeof value === 'bigint' ? value.toString() + 'n' : value
+          );
+          res.status(200).json(json);
           return;
         }
-        getMany<Planet>(path, res);
-        return;
-
-      case ResourcesType.Films:
-        if (id !== null) {
-          getOne<Film>(path, id, res);
-          return;
-        }
-        getMany<Film>(path, res);
-        return;
-
-      case ResourcesType.People:
-        if (id !== null) {
-          getOne<People>(path, id, res);
-          return;
-        }
-        getMany<People>(path, res);
-        return;
-
-      case ResourcesType.Starships:
-        if (id !== null) {
-          getOne<Starship>(path, id, res);
-          return;
-        }
-        getMany<Starship>(path, res);
-        return;
-
-      case ResourcesType.Vehicles:
-        if (id !== null) {
-          getOne<Vehicle>(path, id, res);
-          return;
-        }
-        getMany<Vehicle>(path, res);
-        return;
-
-      case ResourcesType.Species:
-        if (id !== null) {
-          getOne<Species>(path, id, res);
-          return;
-        }
-        getMany<Species>(path, res);
+        const planet = await prisma.planet.findMany();
+        const json = JSON.stringify(planet, (_, value) =>
+          typeof value === 'bigint' ? value.toString() + 'n' : value
+        );
+        res.status(200).json(json);
         return;
 
       default:
@@ -82,46 +52,3 @@ export default function userHandler(
   res.setHeader('Allow', ['GET']);
   res.status(405).end(`Method ${req.method} Not Allowed`);
 }
-
-async function getOne<T>(
-  path: string,
-  id: string,
-  res: NextApiResponse
-): Promise<void> {
-  const result = await getSingleDoc<T>(path, id);
-  if (result.error) {
-    res.status(result.code).json({ message: result.error.message });
-    res.end();
-    return;
-  }
-  res.status(result.code).json(result.data);
-  res.end();
-}
-
-async function getMany<T>(path: string, res: NextApiResponse): Promise<void> {
-  const result = await getCollection<T>(path);
-  if (result.error) {
-    res.status(result.code).json({ message: result.error.message });
-    res.end();
-    return;
-  }
-  if (result.data && Array.isArray(result.data)) {
-    res.status(result.code);
-    res.json(result.data);
-    res.end();
-    return;
-  }
-}
-
-// if (search) {
-//   let matches: any[] = [];
-//   for (let i = 0; i < result.data.length; i++) {
-//     if (result.data[i].name.includes(search as string)) {
-//       matches.push(result.data[i]);
-//     }
-//   }
-//   res.status(result.code);
-//   res.json(matches);
-//   res.end();
-//   return;
-// }
